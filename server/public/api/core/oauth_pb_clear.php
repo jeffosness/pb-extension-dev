5897e66165b9b46fa7cb75e1f35313f93db88f7f
Jeff Osness <47227123+jeffosness@users.noreply.github.com>
2025-12-23 11:16:53 -0700

    Use API bootstrap for PB token save/clear endpoints

--- server/public/api/core/oauth_pb_clear.php
+++ server/public/api/core/oauth_pb_clear.php
@@ -1,11 +1,19 @@
 <?php
-// generic_crm/api/oauth_pb_clear.php
+// server/public/api/core/oauth_pb_clear.php
+//
+// Clears the stored PhoneBurner PAT for this browser client_id.
+
+require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/../../utils.php';
 
-$data = json_input();
+$data      = json_input();
 $client_id = get_client_id_or_fail($data);
 
 // Remove the stored PAT (if any) for this client_id
 clear_pb_token($client_id);
 
-json_response(['ok' => true]);
+api_log('oauth_pb_clear.ok', [
+    'client_id_hash' => substr(hash('sha256', (string)$client_id), 0, 12),
+]);
+
+api_ok([]);
